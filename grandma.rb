9b8c280f4d946d03f9b73c44ee6a866b32629851
Rafael Lopez <lpz.raf@gmail.com>
2018-07-17 14:37:08 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
   if phrase.upcase() == phrase
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "Hi Nana, how are you?"
-    return "NO, NOT SINCE 1938!"
+    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "Hi Nana, how are you?"
     return "NO, NOT SINCE 1938!"
   else 
