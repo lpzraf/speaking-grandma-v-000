15f9ad2d91665fbbd0a13ca4c6ec4416ce5c637d
Rafael Lopez <lpz.raf@gmail.com>
2018-07-17 14:34:48 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase.upcase() == phrase
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase
+  elsif phrase() == phrase
   return "NO, NOT SINCE 1938!"
   else 
   return "HUH?! SPEAK UP, SONNY!"
